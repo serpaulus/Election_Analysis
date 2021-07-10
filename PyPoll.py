11f14bf0e4b8773459548a38b1d60fc5d875677b
SERGIO PARAMO <sergio.paramo@outlook.com>
2021-07-10 15:49:54 -0500

    update txt file

--- PyPoll.py
+++ PyPoll.py
@@ -96,7 +96,9 @@ with open(file_to_save, "w") as txt_file:
         f"Winning Percentage: {winning_percentage:.1f}%\n"
         f"-------------------------\n")
 
-    # print(winning_candidate_summary)
+    print(winning_candidate_summary)
+    # Save the winning candidate's results to the text file.
+    txt_file.write(winning_candidate_summary)
 
 
 
